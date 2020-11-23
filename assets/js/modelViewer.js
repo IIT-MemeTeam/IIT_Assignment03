86e669f154d352cecb65b4c7502f4cda1814d7a2
scuba156RMIT <70957637+scuba156RMIT@users.noreply.github.com>
2020-11-24 00:10:08 +1100

    Add test page for render of 3D model
    - Allows for a rendering of the 3D model of the Mimibox

---
+++ assets/js/modelViewer.js
@@ -0,0 +1,9 @@
+// function onHotSpotClicked(hotspot) {
+//   if (hotspot != null) {
+//       var annotation = hotspot.getElementsByClassName("HotspotAnnotation")[0];
+//
+//       if (annotation != null) {
+//         annotation.toggleAttribute("visible");
+//       }
+//   }
+// }
